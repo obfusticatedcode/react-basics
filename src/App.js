b5446e0d3dd9bf5535abba96e9549502f480d73f
Chisomo Lungu <chisomo@gmail.com>
2018-03-29 22:06:28 +0100

    adding styling via css and inline using js

--- src/App.js
+++ src/App.js
@@ -49,22 +49,31 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white', 
+      font: 'inherit',
+      padding: '8px', 
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button className="button" onClick={() => this.switchNameHandler('Cory')}>Switch Name</button>
+        <button className="button"
+          style = { style }
+          onClick = {() => this.switchNameHandler('Cory')}>Switch Name</button>
         <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}/>
+          name = {this.state.persons[0].name} 
+          age = {this.state.persons[0].age}/>
         <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Grace')}
-          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+          name = {this.state.persons[1].name} 
+          age = {this.state.persons[1].age}
+          click = {this.switchNameHandler.bind(this, 'Grace')}
+          changed = {this.nameChangedHandler}>My Hobbies: Racing</Person>
         <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}/>
+          name = {this.state.persons[2].name} 
+          age = {this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>I'm  {props.name} and {props.age} years old!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
