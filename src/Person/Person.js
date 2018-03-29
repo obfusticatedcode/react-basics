8f204d889491df8b98a3b26448a131f514b419f3
Chisomo Lungu <chisomo@gmail.com>
2018-03-29 15:50:28 +0100

    introduction to state

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const person = (props) => {
-  return <p>I'm  {props.name} and {props.age} years old!</p>
+  return (
+    <div>
+      <p>I'm  {props.name} and {props.age} years old!</p>
+      <p>{props.children}</p>
+    </div>
+    
+    );
 }
 
 export default person;
