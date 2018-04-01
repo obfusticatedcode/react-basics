75a806d180864dc1c8545d7c15ca8966735d5ddf
Chisomo Lungu <chisomo@gmail.com>
2018-04-01 08:51:52 +0100

    using ErrorBoundary

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,13 +2,21 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = (props) => {
+  const rnd = Math.random();
+
+  if (rnd < 0.7) {
+    throw new Error('Something went wrong');
+  }
+
   return (
     <div className={classes.Person}>
-      <p onClick={props.click}>I'm  {props.name} and {props.age} years old!</p>
+      <p onClick={props.click}>I'm {props.name}
+        and {props.age}
+        years old!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
-    </div>   
-    );
+    </div>
+  );
 }
 
 export default person;
