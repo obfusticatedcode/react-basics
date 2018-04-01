73b1a78f8b05ffb1f36fd10429b5840dfa7ce79d
Chisomo Lungu <chisomo@gmail.com>
2018-04-01 16:48:49 +0100

    converting stateless compts to stateful compts

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I'm {props.name} and {props.age} years old!</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-  );
+class Person extends Component {
+  render() {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I'm {this.props.name}
+          and {this.props.age}
+          years old!</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+      </div>
+    );
+  }
 }
 
-export default person;
+export default Person;
