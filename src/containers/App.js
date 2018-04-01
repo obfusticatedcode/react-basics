7e1e89b46f29d82055e581491f779627a3d830d1
Chisomo Lungu <chisomo@gmail.com>
2018-04-01 16:31:15 +0100

    using this.props

--- src/containers/App.js
+++ src/containers/App.js
@@ -67,7 +67,8 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <Cockpit 
+        <Cockpit
+          appTitle={this.props.title} 
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}/>
